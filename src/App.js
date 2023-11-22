df2b978be51feb2557acf5744835ec226bba439f
Matteo Pisu <151760470+matte261@users.noreply.github.com>
2023-11-22 16:02:56 +0000

    Update 5 files

--- src/App.js
+++ src/App.js
@@ -1,32 +1,73 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
+import React, { useState } from "react";
+import styled from "styled-components";
+
+import logo from "./res/insta.png";
 
 //Dichiarazione Styled Components:
 const Div = styled.div`
-  margin: 100px auto;
-  padding: 50px;
-  background-color: #66A182;
-  border: 2px solid #2E4057;
-  max-width: 700px;
+  background-color: grey;
+  border: 2px solid #2e4057;
   border-radius: 5px;
   display: flex;
   flex-direction: column;
+  width: 100%;
+  height: 100%;
 `;
 
-const H1 = styled.h1`
-  color: #2E4057;
+const Logo = styled.img`
+  width: 3rem;
   margin: auto;
+  margin-left: 0;
+  filter: invert(0);
+  @media only screen and (max-width: 900px) {
+    width: 2rem;
+  }
+`;
+
+const Container = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: row;
+  padding-top: 50px;
+  background-color: grey;
+`;
+
+const Element = styled.p`
+  font-size: 30px;
+  color: orange;
+  list-style: none;
+  margin: 20px;
+  transition: 0;
+  padding: 20px;
+`;
+
+const Wrapper = styled.div`
+  width: 100%;
+  background-color: #1d1d1d;
+  display: flex;
+  padding: 20px;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+const title = styled.h1`
+  width: 75%;
+  margin: auto;
+  margin-bottom: 0;
   text-align: center;
-  font-size: 70px;
+  font-size: 4rem;
 `;
 
 const App = () => {
-
-  return(
+  return (
     <Div>
-      <H1>Hello world</H1>
+      <Wrapper>
+        <Logo src={logo} className="Logo" />
+        <Element> Matteo Pisu </Element>
+      </Wrapper>
+      <Container>corpo</Container>
     </Div>
-  )
+  );
 };
 
 export default App;
